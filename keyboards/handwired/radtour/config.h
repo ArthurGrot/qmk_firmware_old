4730fbd1722e317ea22b17e488036e73ce52e199
Radtour <46630208+Radtour@users.noreply.github.com>
2020-09-26 18:30:44 +0200

    Moved files to handwired/radtour

--- keyboards/handwired/radtour/config.h
+++ keyboards/handwired/radtour/config.h
@@ -4,20 +4,25 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    qmkbuilder
-#define PRODUCT         Arthur_Tastatur
-#define DESCRIPTION     DIY Keyboard
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0x6060
+#define DEVICE_VER 0x0001
+#define MANUFACTURER qmkbuilder
+#define PRODUCT Arthur_Tastatur
+#define DESCRIPTION DIY Keyboard
+
+/* power settings */
+#define USB_MAX_POWER_CONSUMPTION 100
 
 /* key matrix size */
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 18
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { D4, D5, D7, E0, E1, C0 }
-#define MATRIX_COL_PINS { B1, B2, B3, A0, B0, F5, E6, A1, A2, F7, A7, A6, A5, A4, C7, C3, C2, C1 }
+#define MATRIX_ROW_PINS \
+    { D4, D5, D7, E0, E1, C0 }
+#define MATRIX_COL_PINS \
+    { B1, B2, B3, A0, B0, F5, E6, A1, A2, F7, A7, A6, A5, A4, C7, C3, C2, C1 }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
@@ -26,7 +31,7 @@
 /* number of backlight levels */
 
 #ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 0
+#    define BACKLIGHT_LEVELS 0
 #endif
 
 /* Set 0 if debouncing isn't needed */
@@ -39,20 +44,17 @@
 #define LOCKING_RESYNC_ENABLE
 
 /* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
-)
+#define IS_COMMAND() (keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
 
 /* prevent stuck modifiers */
 #define PREVENT_STUCK_MODIFIERS
 
-
 #ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 0
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLED_NUM 0
+#    define RGBLIGHT_HUE_STEP 8
+#    define RGBLIGHT_SAT_STEP 8
+#    define RGBLIGHT_VAL_STEP 8
 #endif
 
 #endif
--- keyboards/handwired/radtour/radtour.c
+++ keyboards/handwired/radtour/radtour.c
@@ -1,4 +1,4 @@
-#include "kb.h"
+#include "radtour.h"
 
 void keyboard_pre_init_user(void) {
 	setPinOutput(D6);
